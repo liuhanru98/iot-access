127a9e17dcf89531d21277bcf66767651cb3bfea
liuhanru <m18401773135@163.com>
2019-06-20 16:12:31 +0800

    接口初步完成提交

--- iot-access-proxy/src/test/java/com/shenzhen/teamway/iotaccessproxy/IotAccessProxyApplicationTests.java
+++ iot-access-proxy/src/test/java/com/shenzhen/teamway/iotaccessproxy/IotAccessProxyApplicationTests.java
@@ -1,16 +1,22 @@
 package com.shenzhen.teamway.iotaccessproxy;
 
+import com.shenzhen.teamway.iotaccessproxy.controller.IotAccessController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = IotAccessProxyApplication.class)
 public class IotAccessProxyApplicationTests {
+
+  @Autowired
+  private IotAccessController iotAccessController;
 
   @Test
   public void contextLoads() {
+    iotAccessController.getCountPersonOfChannel();
   }
 
 }
